17cfe9f0cec9e42b65d685f51df6ee03b920521e
Unknown <calosguilherme@hotmail.com>
2019-06-17 19:21:51 -0300

    Ajuste Heroku

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = process.env.npm_package_name;
 const app = express();
  
-app.use(express.static(`${__dirname}/dist/locadora-frontend`));
+app.use(express.static(__dirname+'/dist/locadora-frontend'));
  
-app.get('/*', function(req,res) => {
-res.sendFile(path.join(`${__dirname}/dist/locadora-frontend/index.html`));
+app.get('/*', function (req, res) {
+
+  res.sendFile(path.join(__dirname + '/dist/locadora-frontend/index.html'));
 });
- 
-app.listen(process.env.PORT || 8080)
+
+app.listen(process.env.PORT || 8080);
