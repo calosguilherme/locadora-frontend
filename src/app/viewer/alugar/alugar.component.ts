bff465e6d6ae8fa3cae1ca2440c34a10a329a540
joseslima <guislima47@gmail.com>
2019-07-01 01:41:26 -0300

    suporte para boleto

--- src/app/viewer/alugar/alugar.component.ts
+++ src/app/viewer/alugar/alugar.component.ts
@@ -39,6 +39,9 @@ export class AlugarComponent implements OnInit {
   public metodopagamento: number
   public sucRequi: boolean = false;
   public preco: number;
+  public boleto_barcode: string;
+  public boleto_url: string;
+  public boleto: boolean = false;
   public avaliacao: number[]
   public pessoaJogo: PessoaJogo
   public dataLocacao: Date
@@ -117,9 +120,19 @@ export class AlugarComponent implements OnInit {
       idpessoa,
       idjogo
     }
-        
+    console.log(body);
+    
     this.locacaoService.create(body).subscribe(
       success => {
+        const {boleto} = success;
+
+        if (boleto.url) {
+          this.boleto = true
+          this.boleto_url = boleto.url
+          this.boleto_barcode = boleto.codigo
+
+        }
+        
         this.messageService.add({ severity: 'success', summary: 'Sucesso', detail: success.message });
       },
       error => {
@@ -127,8 +140,25 @@ export class AlugarComponent implements OnInit {
       }
     )
   }
-  
+  copyInputMessage(inputElement){
+    inputElement.select();
+    document.execCommand('copy');
+    inputElement.setSelectionRange(0, 0);
+  }
 
+  copyText(val: string){
+    let selBox = document.createElement('textarea');
+      selBox.style.position = 'fixed';
+      selBox.style.left = '0';
+      selBox.style.top = '0';
+      selBox.style.opacity = '0';
+      selBox.value = val;
+      document.body.appendChild(selBox);
+      selBox.focus();
+      selBox.select();
+      document.execCommand('copy');
+      document.body.removeChild(selBox);
+    }
   listarPessoaJogo(jogo) {
     this.modalJogo = jogo
     
@@ -280,7 +310,6 @@ export class AlugarComponent implements OnInit {
     this.pessoaJogoService.create(pessoaJogo).subscribe(
       success => {
         this.cadPessoaJogo = false;
-        console.log(success);
         this.messageService.add({
           severity: "success",
           summary: "Sucesso",
